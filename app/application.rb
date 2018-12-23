f3c867ec26a0bef7a355241a27560728590614a2
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-23 20:13:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,18 +5,19 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
-    @@items.each do |item|
-      
-      if @@items.include?(item)
-        resp.write "#{item.price}"
-        elsif @@items.exclude?(item)
+    if req.path.match(/items/)
+        @@items.each do |item|
+        if @@items.include?(item) 
+          resp.write "#{item.price}"
+        else
            resp.write "Item not found"
            resp.status = 400
+         end 
+      end 
             else
              resp.write "Route not found"
              resp.status = 404
     end
     resp.finish
-    
 end
 end 
