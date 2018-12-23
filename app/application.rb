757ad849c465def0b14884abe29d973e233ae428
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-23 20:02:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,12 +13,11 @@ class Application
            resp.write "Item not found"
            resp.status = 400
          end 
-     
+      end 
             else
              resp.write "Route not found"
              resp.status = 404
     end
-  end 
     resp.finish
 end
 end 
