0ca416b3119254389611b97fb4099acbe28a425a
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-23 18:47:25 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+class Application
+  @@items = Item.all
+
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path== "/items/ #{item.name}"
+      @@items.each do |item|
+      resp.write "#{item.price}"
+      end 
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  
+end
+
+
+end 
