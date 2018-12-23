936738d54edca3f987df6fe5b7c7d2bdec0398fc
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-23 19:17:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
-    if req.path== "/items/ #{@@item.name}"
-      @@items.each do |item|
+
+    @@items.each do |item|
+    if req.path== "/items/ #{item.name}"
       resp.write "#{item.price}"
       end 
     else
