f89537943fa42cd2922c4649b396815f2459aa80
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-23 19:50:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
  
     if req.path.match(/items/)
       @@items.each do |item|
-        if @@item.include?(item) 
+        if @@items.include?(item) 
           resp.write "#{item.price}"
         else 
            resp.status = 400
